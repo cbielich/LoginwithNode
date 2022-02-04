e792d2a399ba2498328a06c1ab326786a4052599
Cesar Bielich <cesarbielich@gmail.com>
2022-02-04 15:16:19 -0800

    More stuff

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-const express = require('express');
-const app = express();
-const bcrypt = require('bcrypt');
-const res = require('express/lib/response');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const res = require('express/lib/response')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializePassport = require('./password-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
 
 const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -16,17 +38,20 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
 
-})
+}))
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', async (req, rez) => {
+app.post('/register', async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
         users.push({
             id: Date.now().toString(),
             name: req.body.name,
@@ -37,7 +62,6 @@ app.post('/register', async (req, rez) => {
     } catch {
         res.redirect('/register')
     }
-    // console.log(users)
 })
 
 app.listen(8080)
